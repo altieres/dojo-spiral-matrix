f3ab9f02ce065ee206f013200baf4310f0c4ffff
Altieres <altiereslopes@webgoal.com.br>
2013-02-21 21:21:17 -0300

    progress

--- test.py
+++ test.py
@@ -2,16 +2,27 @@ import unittest
 
 class TestSequenceFunctions(unittest.TestCase):
 
-	def test_oneOne(self):
+	def testOneOne(self):
 		expected = [[1]]
-		matrix = Matrix()
+		matrix = Matrix(1)
+		self.assertEquals(expected, matrix.compute())
+		
+	def testOneThree(self):
+		expected = [[1, 2, 3]]
+		matrix = Matrix(3)
 		self.assertEquals(expected, matrix.compute())
 		
 
 class Matrix:
 	
+	def __init__(self, columns):
+		self.columns = columns
+		
 	def compute(self):
-		return [[1]]
+		matrix = [[0 for i in range(self.columns)]]
+		for column in range(self.columns):
+			matrix[0][column] = column + 1
+		return matrix
 		
 
 if __name__ == '__main__':
