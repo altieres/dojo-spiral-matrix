34ae1d10dda6b0b61c11bc63835d8c5c56d056c4
Altieres <altiereslopes@webgoal.com.br>
2013-02-21 21:33:20 -0300

    progress

--- test.py
+++ test.py
@@ -12,18 +12,24 @@ class TestSequenceFunctions(unittest.TestCase):
 		matrix = Matrix(1, 3)
 		self.assertEquals(expected, matrix.compute())
 		
+	def testLastColumn(self):
+		matrix = Matrix(3, 3).compute()
+		for line in range(3):
+			self.assertEquals(line+3, matrix[line][2])
+			
 
 class Matrix:
 	
 	def __init__(self, lines, columns):
 		self.lines = lines
 		self.columns = columns
+		self.matrix = [[0 for i in range(self.columns)] for i in range(self.lines)]
 		
 	def compute(self):
-		matrix = [[0 for i in range(self.columns)]]
-		for column in range(self.columns):
-			matrix[0][column] = column + 1
-		return matrix
+		for line in range(self.lines):
+			for column in range(self.columns):
+				self.matrix[line][column] = column + line + 1
+		return self.matrix
 		
 
 if __name__ == '__main__':
